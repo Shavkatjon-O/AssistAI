0776b24c3a3abd983f79354f173c3e46d24cbcbf
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-12-30 19:46:39 +0500

    feat: add langchain packages, create simple chatbot interface using openai api

---
+++ backend/chain.py
@@ -0,0 +1,21 @@
+from langchain_openai import ChatOpenAI
+
+if __name__ == "__main__":
+    import os
+
+    from dotenv import find_dotenv, load_dotenv
+
+    _ = load_dotenv(find_dotenv())
+
+    if os.getenv("OPENAI_API_KEY") is None:
+        print("Please set OPENAI_API_KEY environment variable")
+        exit(1)
+
+    model = ChatOpenAI(model="gpt-4o-mini")
+
+    while True:
+        prompt = input("Enter a prompt: ")
+        if prompt == "exit":
+            break
+        response = model.invoke(prompt)
+        print(response.content)
