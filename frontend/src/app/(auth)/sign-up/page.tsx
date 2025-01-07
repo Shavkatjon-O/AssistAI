e5ed6b51e8fe6cfdd6a59473240f67657f8f3ade
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2025-01-07 18:11:45 +0500

    fix bugs

--- frontend/src/app/(auth)/sign-up/page.tsx
+++
@@ -1,4 +0,0 @@
-const Page = () => {
-  return <div>Sign Up</div>;
-};
-export default Page;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,21 @@
+import Image from "next/image";
+
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
+        <Image
+          src="/assistai.png"
+          alt="AssistAI Logo"
+          width={256}
+          height={256}
+          className="animate-bounce"
+        />
+        <div>
+          Coming Soon ...
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Page;
